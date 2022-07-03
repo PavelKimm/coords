64fb9bf698712281cc770e0939c6f435aa6534a2
Pavel <fastredialer@gmail.com>
2022-07-03 14:32:02 +0600

    api save_route method

--- routes/urls.py
+++ routes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from routes.views import get_directions
+from routes.views import get_directions, save_route, PointListView
 
 urlpatterns = [
-    path('points/', get_directions),
+    path('points/', PointListView.as_view()),
     path('get-directions/', get_directions),
+
+    path('save-route/', save_route)
 ]
--- routes/views.py
+++ routes/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -5,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 from coords.pagination import DefaultPagination
-from routes.models import Point, Route
+from routes.models import Point, Route, UserRoute
 from routes.serializers import PointListSerializer
 
 
@@ -31,3 +32,25 @@ def get_directions(request):
     except Exception as e:
         return Response({"detail": str(e)}, status=HTTP_400_BAD_REQUEST)
     return Response({'message': route_points}, status=HTTP_200_OK)
+
+
+@api_view(["POST"])
+@permission_classes((IsAuthenticated,))
+def save_route(request):
+    """
+    save points in route object
+    """
+    route_number = request.data.get('route_number')
+    points = request.data.get('points')
+    if not route_number:
+        return Response({"detail": 'route_number must be provided'}, status=HTTP_400_BAD_REQUEST)
+    if not points:
+        return Response({"detail": 'points must be provided'}, status=HTTP_400_BAD_REQUEST)
+    try:
+        with transaction.atomic():
+            route = Route.objects.create(number=route_number)
+            route.points.set(points)
+            user_route = UserRoute.objects.create(user=request.user, route=route)
+    except Exception as e:
+        return Response({"detail": str(e)}, status=HTTP_400_BAD_REQUEST)
+    return Response({'message': f'{user_route} was created'}, status=HTTP_200_OK)
